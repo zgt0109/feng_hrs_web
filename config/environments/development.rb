0639990271516776b1c4a6c6bd128ceba767a2b1
guxiaobai <sikuan.gu@gmail.com>
2015-11-30 13:44:01 +0800

    oh-my-rails: add guard

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload
   config.action_mailer.delivery_method = :letter_opener
   # Settings specified here will take precedence over those in config/application.rb.
 
