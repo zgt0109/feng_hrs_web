91608958a17d64b68c57f15130fccbe831191716
guxiaobai <sikuan.gu@gmail.com>
2015-11-30 13:43:41 +0800

    oh-my-rails: add capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,61 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+set :application, 'feng_hrs_web'
+set :repo_url, 'git@github.com:guxiaobai/feng_hrs_web.git'
+
+# If the environment differs from the stage name
+set :rails_env, 'production'
+
+# Default branch is :master
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/var/www/feng_hrs_web'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# https://github.com/capistrano/bundler
+set :bundle_jobs, 4 # default: nil, only available for Bundler >= 1.4
+
+# https://github.com/capistrano/rbenv
+set :rbenv_type, :system # or :user, depends on your rbenv setup
+set :rbenv_ruby, '2.2.2'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
+
+namespace :deploy do
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
