5c169d9f187cec6a7b9e429563fa1680e145e352
guxiaobai <sikuan.gu@gmail.com>
2015-11-30 15:54:21 +0800

    新增企业用户模板

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require semantic-ui/dist/semantic
+$(function(){
+  $('.ui.dropdown').dropdown({
+    on: 'hover'
+  });
+
+  $('.ui.sidebar').sidebar({
+    context: $('.bottom.segment')
+  }).sidebar('attach events', '#tools .item:eq(1)');
+
+})
