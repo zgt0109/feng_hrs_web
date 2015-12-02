83b5663d470b0437e7c79fe7984ebe43c71e3c7a
guxiaobai <sikuan.gu@gmail.com>
2015-12-02 18:55:31 +0800

    后台模版调整

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,19 @@
 //= require semantic-ui/dist/semantic
 //= require_directory ./plugins
 //= require 'china_city/jquery.china_city'
+
+// semantic-ui
+$(function(){
+  $('.ui.dropdown').dropdown({
+    on: 'hover'
+  });
+
+  $('.ui.sidebar').sidebar({
+    context: $('.bottom.segment')
+  }).sidebar('attach events', '#tools .item:eq(1)');
+
+  $(':checkbox[name=selectAll]').on('change', function() {
+    $(':checkbox[name=singular_check]').prop('checked', this.checked);
+  })
+
+})
--- app/assets/javascripts/enterprise.js
+++ app/assets/javascripts/enterprise.js
@@ -1,15 +1,1 @@
-// semantic-ui
-$(function(){
-  $('.ui.dropdown').dropdown({
-    on: 'hover'
-  });
 
-  $('.ui.sidebar').sidebar({
-    context: $('.bottom.segment')
-  }).sidebar('attach events', '#tools .item:eq(1)');
-
-  $(':checkbox[name=selectAll]').on('change', function() {
-    $(':checkbox[name=singular_check]').prop('checked', this.checked);
-  })
-
-})
