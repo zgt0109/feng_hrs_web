4103e18028508492f57865052455b201524ce054
guxiaobai <sikuan.gu@gmail.com>
2015-12-10 19:56:14 +0800

    轮播菜单

---
+++ app/assets/javascripts/applicant.js
@@ -0,0 +1,14 @@
+//= require slick-carousel/slick/slick.js
+
+$(function(){
+
+  // banner
+  $('.slick').slick({
+    autoplay: true,
+    fade: true,
+    arrows: false,
+    dots: true,             // 当前幻灯片指标点
+    // dotsClass: 'slick-dots',  // 指标点容器
+
+  });
+})
