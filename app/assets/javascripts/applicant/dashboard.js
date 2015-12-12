0d89eb93ec6b1bb121de97eb69aa397d4a85e92f
guxiaobai <sikuan.gu@gmail.com>
2015-12-12 16:51:52 +0800

    首页调整

---
+++ app/assets/javascripts/applicant/dashboard.js
@@ -0,0 +1,12 @@
+//= require slick-carousel/slick/slick.js
+
+$(function(){
+
+  // banner
+  $('.slick').slick({
+    autoplay: true,
+    fade: true,
+    arrows: false,
+    dots: true,
+  });
+})
