4f5d6a7c03885ed95346db0472513c5632dc86e2
zgt <2506526350@qq.com>
2015-12-15 18:41:17 +0800

    Enterprise下面点击左上角Logo回到首页

--- app/assets/javascripts/enterprise.js
+++ app/assets/javascripts/enterprise.js
@@ -1,3 +1,9 @@
 //= require cocoon
 //= require plupload/js/plupload.full.min.js
 //= require qiniu/src/qiniu.js
+
+$(function (){
+  $('#tools .item.header').click(function() {
+    window.location.href= '/'
+  })
+})
